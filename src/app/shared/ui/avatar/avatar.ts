fa96b3f06343d990d2892b1e8ccf49b29af20574
Wilfried Musanzi <musanziw.m@gmail.com>
2025-12-18 09:16:25 +0200

    refactor(avatar): simplify avatar component structure and improve styling for better visual consistency

--- src/app/shared/ui/avatar/avatar.ts
+++ src/app/shared/ui/avatar/avatar.ts
@@ -1,17 +1,14 @@
 import { Component, input } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
-import { ImageOff, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-ui-avatar',
-  imports: [NgOptimizedImage, LucideAngularModule],
+  imports: [NgOptimizedImage],
   templateUrl: './avatar.html'
 })
 export class UiAvatar {
   label = input<string>('');
   image = input<string | null>(null);
-  size = input<number>(50);
-  icons = { ImageOff };
   shape = input<'circle' | 'square'>('circle');
 
   avatarClasses() {
