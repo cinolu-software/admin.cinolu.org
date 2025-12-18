9e2ba83a5a887aa2bf56844d8b23455974a07ce1
Wilfried Musanzi <musanziw.m@gmail.com>
2025-12-18 08:42:46 +0200

    refactor(sign-in): simplify sign-in page layout by removing unused icon and adjusting styling

--- src/app/features/sign-in/pages/sign-in.ts
+++ src/app/features/sign-in/pages/sign-in.ts
@@ -1,24 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgOptimizedImage } from '@angular/common';
-import { LucideAngularModule, CircleAlert } from 'lucide-angular';
 import { UiInput, UiPassword, UiButton } from '@ui';
 import { SignInStore } from '../store/sign-in.store';
 import { AuthStore } from '@core/auth/auth.store';
-import { environment } from '@env/environment';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.html',
   providers: [SignInStore],
-  imports: [ReactiveFormsModule, NgOptimizedImage, LucideAngularModule, UiInput, UiPassword, UiButton]
+  imports: [ReactiveFormsModule, NgOptimizedImage, UiInput, UiPassword, UiButton]
 })
 export class SignIn {
   #formBuilder: FormBuilder = inject(FormBuilder);
   form: FormGroup;
   store = inject(SignInStore);
   authStore = inject(AuthStore);
-  icons = { CircleAlert };
 
   constructor() {
     this.form = this.#formBuilder.group({
@@ -33,9 +30,5 @@ export class SignIn {
       payload: this.form.value,
       onSuccess: () => true
     });
-  }
-
-  signinWithGoogle(): void {
-    window.location.replace(environment.apiUrl + 'auth/sign-in');
   }
 }
