445e7de65c3913e77db8aae68c92e798f0aa4bc7
Wilfried Musanzi <musanziw.m@gmail.com>
2025-12-18 09:15:55 +0200

    refactor(desktop-nav): remove unused CommonModule import for cleaner code

--- src/app/layout/components/topbar/desktop-menu/desktop-nav.ts
+++ src/app/layout/components/topbar/desktop-menu/desktop-nav.ts
@@ -1,5 +1,4 @@
 import { Component, input, output } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LucideAngularModule, LogOut, ChevronDown } from 'lucide-angular';
 import { IUser } from '@shared/models';
@@ -9,7 +8,7 @@ import { UiAvatar } from '@ui';
 @Component({
   selector: 'app-desktop-nav',
   templateUrl: './desktop-nav.html',
-  imports: [CommonModule, RouterModule, ApiImgPipe, LucideAngularModule, UiAvatar]
+  imports: [RouterModule, ApiImgPipe, LucideAngularModule, UiAvatar]
 })
 export class DesktopNav {
   user = input.required<IUser | null>();
