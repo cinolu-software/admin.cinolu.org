0029fa92ed014a7195db63e5496099f75f4e2935
Wilfried Musanzi <musanziw.m@gmail.com>
2026-02-04 15:36:52 +0200

    refactor(participants): remove unused participants DTO and store implementation

--- src/app/features/projects/dto/participants/participants.dto.ts
+++
@@ -1,7 +0,0 @@
-import type { IPhase } from '@shared/models';
-import type { IUser } from '@shared/models';
-
-export interface ParticipantsGroupedByPhaseDto {
-  phases: { phase: IPhase; participants: IUser[] }[];
-  unassigned: IUser[];
-}
